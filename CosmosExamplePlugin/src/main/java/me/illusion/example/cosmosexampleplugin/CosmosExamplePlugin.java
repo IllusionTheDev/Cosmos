f94b78313d674b7f1291a36659cd1df4fcac2898
Illusion <imdatillusion@gmail.com>
2023-04-29 23:53:04 +0100

    More comments

--- CosmosExamplePlugin/src/main/java/me/illusion/example/cosmosexampleplugin/CosmosExamplePlugin.java
+++ CosmosExamplePlugin/src/main/java/me/illusion/example/cosmosexampleplugin/CosmosExamplePlugin.java
@@ -38,10 +38,15 @@ public final class CosmosExamplePlugin extends JavaPlugin {
     }
 
     private void initCosmos() {
+        // On skyblock, we'll use a world per area grid, with a maximum of 25 active worlds at a time.
+        // A world per area grid will create a new world for each area, automatically loading, unloading and deleting them as needed.
         CosmosGrid grid = WorldPerAreaGrid.builder().maxActiveWorlds(25).build();
+
+        // Register the grid
         cosmosPlugin.getGridRegistry().register(grid);
 
-        sessionHolder = new CosmosSessionHolder(cosmosPlugin.getContainerRegistry().getDefaultContainer(), grid); // Save on the default container, and use the grid we just registered
+        // Save on the default container, and use the grid we just registered
+        sessionHolder = new CosmosSessionHolder(cosmosPlugin.getContainerRegistry().getDefaultContainer(), grid);
 
         // Attempt to fetch the template from the database and cache it, if it isn't found, it simply won't cache.
         cosmosPlugin.getTemplateCache().register("skyblock", cosmosPlugin.getContainerRegistry().getDefaultContainer().fetchTemplate("skyblock"));
@@ -49,6 +54,7 @@ public final class CosmosExamplePlugin extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        // We'll unload all the worlds when the plugin is disabled. We join the completable future to ensure it's completed before the plugin is disabled.
         sessionHolder.unloadAll().join();
     }
 }
