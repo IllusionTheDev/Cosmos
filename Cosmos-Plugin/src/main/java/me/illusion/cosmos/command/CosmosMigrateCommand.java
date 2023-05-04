fe15a4e5252226eabd53d775910aab7902ad74a4
Harold <richardli888999@gmail.com>
2023-05-03 22:10:36 -0400

    changed initial pool to 1 (temporarily) so i don't have to delete 5 pool worlds after every restart,
    finished placeholder impl
    
    fixed typo in messages.yml

--- Cosmos-Plugin/src/main/java/me/illusion/cosmos/command/CosmosMigrateCommand.java
+++ Cosmos-Plugin/src/main/java/me/illusion/cosmos/command/CosmosMigrateCommand.java
@@ -1,13 +1,13 @@
 package me.illusion.cosmos.command;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import me.illusion.cosmos.CosmosPlugin;
 import me.illusion.cosmos.database.CosmosDataContainer;
 import me.illusion.cosmos.utilities.command.SimpleCommand;
 import me.illusion.cosmos.utilities.text.Placeholder;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 public class CosmosMigrateCommand implements SimpleCommand {
@@ -52,22 +52,34 @@ public class CosmosMigrateCommand implements SimpleCommand {
             return;
         }
 
-        List<Placeholder<?>> placeholders = Arrays.asList(
-                new Placeholder<>("%template%", templateName),
-                new Placeholder<>("%source%", source),
-                new Placeholder<>("%destination%", destination)
-        );
+//        List<Placeholder<?>> placeholders = Arrays.asList(
+//                new Placeholder<>("%template%", templateName),
+//                new Placeholder<>("%source%", source),
+//                new Placeholder<>("%destination%", destination)
+//        );
+
+        Placeholder<CommandSender> sourcePlaceholder = new Placeholder<>("%source%", source);
+        Placeholder<CommandSender> destinationPlaceholder = new Placeholder<>("%destination%", destination);
+        Placeholder<CommandSender> templatePlaceholder = new Placeholder<>("%template%", templateName);
 
         CosmosDataContainer sourceContainer = plugin.getContainerRegistry().getContainer(source);
         CosmosDataContainer destinationContainer = plugin.getContainerRegistry().getContainer(destination);
 
         if (sourceContainer == null) {
-            plugin.getMessages().sendMessage(sender, "migrate.invalid-source");
+            plugin.getMessages().sendMessage(
+                    sender,
+                    "migrate.invalid-source",
+                    sourcePlaceholder
+                    );
             return;
         }
 
         if (destinationContainer == null) {
-            plugin.getMessages().sendMessage(sender, "migrate.invalid-destination");
+            plugin.getMessages().sendMessage(
+                    sender,
+                    "migrate.invalid-destination",
+                    destinationPlaceholder
+            );
             return;
         }
 
@@ -83,29 +95,43 @@ public class CosmosMigrateCommand implements SimpleCommand {
                 CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).thenRun(() -> {
                     plugin.getMessages().sendMessage(
                             sender,
-                            "migrate.success"
+                            "migrate.success",
+                            sourcePlaceholder,
+                            destinationPlaceholder
                     );
                 });
             });
 
             plugin.getMessages().sendMessage(
                     sender,
-                    "migrate.started"
-//                    new Placeholder<>("%source%", source),
-//                    new Placeholder<>("%destination%", destination)
+                    "migrate.started",
+                    sourcePlaceholder,
+                    destinationPlaceholder
             );
             return;
         }
 
         sourceContainer.fetchTemplate(templateName).thenCompose(template -> {
             if (template == null) {
-                plugin.getMessages().sendMessage(sender, "migrate.invalid-template");
+                plugin.getMessages().sendMessage(sender,
+                        "migrate.invalid-template",
+                        templatePlaceholder
+                );
                 return CompletableFuture.completedFuture(null);
             }
 
-            return destinationContainer.saveTemplate(templateName, template).thenRun(() -> plugin.getMessages().sendMessage(sender, "migrate.success"));
+            return destinationContainer.saveTemplate(templateName, template).thenRun(() -> plugin.getMessages().sendMessage(
+                    sender,
+                    "migrate.success",
+                    sourcePlaceholder,
+                    destinationPlaceholder));
         });
 
-        plugin.getMessages().sendMessage(sender, "migrate.started");
+        plugin.getMessages().sendMessage(
+                sender,
+                "migrate.started",
+                sourcePlaceholder,
+                destinationPlaceholder
+        );
     }
 }
