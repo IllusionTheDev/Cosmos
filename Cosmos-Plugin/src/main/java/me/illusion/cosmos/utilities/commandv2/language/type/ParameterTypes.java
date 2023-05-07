afdf0a8d6bfb837e2c46b48a986c5e440e673f8a
Illusion <imdatillusion@gmail.com>
2023-05-07 01:26:39 +0100

    Removed something that shouldn't have been here

--- Cosmos-Plugin/src/main/java/me/illusion/cosmos/utilities/commandv2/language/type/ParameterTypes.java
+++ Cosmos-Plugin/src/main/java/me/illusion/cosmos/utilities/commandv2/language/type/ParameterTypes.java
@@ -4,7 +4,6 @@ import me.illusion.cosmos.utilities.commandv2.language.type.impl.IntegerParamete
 import me.illusion.cosmos.utilities.commandv2.language.type.impl.NumericalParameterType;
 import me.illusion.cosmos.utilities.commandv2.language.type.impl.StringParameterType;
 import me.illusion.cosmos.utilities.commandv2.language.type.impl.filtered.TimeParameterType;
-import me.illusion.cosmos.utilities.commandv2.language.type.impl.smart.ConditionParameterType;
 import me.illusion.cosmos.utilities.commandv2.language.unit.MinecraftTime;
 
 public class ParameterTypes {
@@ -13,6 +12,5 @@ public class ParameterTypes {
     public static final ParameterType<Integer> INTEGER = new IntegerParameterType();
     public static final ParameterType<Double> NUMERICAL = new NumericalParameterType();
     public static final ParameterType<MinecraftTime> MINECRAFT_TIME = new TimeParameterType();
-    public static final ParameterType<String> CONDITION = new ConditionParameterType();
 
 }
